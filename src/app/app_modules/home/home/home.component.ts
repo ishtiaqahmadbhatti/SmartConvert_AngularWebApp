a43f78ca2ee00f0555a9f0402835cb333c4e7f1e
Ishtiaq Ahmad <ishtiaq.uaar55@gmail.com>
2025-01-03 02:23:47 +0500

    Header Footer and Home Component UI Added

--- src/app/app_modules/home/home/home.component.ts
+++ src/app/app_modules/home/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../../app_layouts/header/header.component";
+import { FooterComponent } from "../../../app_layouts/footer/footer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
